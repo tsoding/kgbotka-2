4e9c0338748acf81c57dd68827e4b6f442f5ca8a
rexim <reximkut@gmail.com>
2021-03-13 12:03:55 +0700

    Remove commands

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
 SSL_CFLAGS=`pkg-config --cflags openssl`
 SSL_LIBS=`pkg-config --libs openssl`
 
-cc $CFLAGS $SSL_CFLAGS -o kgbotka src/main.c src/arena.c src/sv.c src/command.c src/log.c src/secret.c src/irc.c -lm $SSL_LIBS
+cc $CFLAGS $SSL_CFLAGS -o kgbotka src/main.c src/arena.c src/sv.c src/log.c src/secret.c src/irc.c -lm $SSL_LIBS
--- src/main.c
+++ src/main.c
@@ -7,15 +7,12 @@
 #include <ctype.h>
 
 #include "./arena.h"
-#include "./command.h"
 #include "./log.h"
 #include "./secret.h"
 #include "./irc.h"
 
 #define HOST "irc.chat.twitch.tv"
-// #define PORT "6667"
 #define PORT "6697"
-#define SECRET_CONF "secret.conf"
 
 char *shift(int *argc, char ***argv)
 {
@@ -28,7 +25,7 @@ char *shift(int *argc, char ***argv)
 
 void usage(const char *program, FILE *stream)
 {
-    fprintf(stream, "Usage: %s <state/> <secret.conf>\n", program);
+    fprintf(stream, "Usage: %s <secret.conf>\n", program);
 }
 
 Arena secret_arena = {0};
@@ -44,14 +41,6 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    const String_View state_path = sv_from_cstr(shift(&argc, &argv));
-
-    if (argc == 0) {
-        usage(program, stderr);
-        fprintf(stderr, "ERROR: path to secret.conf file expected\n");
-        exit(1);
-    }
-
     const String_View secret_conf_path = sv_from_cstr(shift(&argc, &argv));
 
     Secret secret = secret_from_file(&secret_arena, secret_conf_path);
@@ -63,20 +52,6 @@ int main(int argc, char **argv)
     irc_pass(&irc, secret.password);
     irc_nick(&irc, secret.nickname);
     irc_join(&irc, secret.channel);
-
-    // TODO: hot reloading of the commands is not implemented
-    Commands commands = {0};
-    arena_clean(&commands_arena);
-    String_View commands_file_path = SV_CONCAT(&commands_arena, state_path, SV("/commands.txt"));
-    if (!load_commands_file(&commands_arena, &log, commands_file_path, &commands)) {
-        arena_clean(&commands_arena);
-        memset(&commands, 0, sizeof(commands));
-    } else {
-        log_info(&log, "Loaded %zu commands", commands.count);
-        for (size_t i = 0; i < commands.count; ++i) {
-            log_info(&log, "    "SV_Fmt, SV_Arg(commands.command_defs[i].name));
-        }
-    }
 
     // TODO: autoreconnect
 
@@ -122,16 +97,6 @@ int main(int argc, char **argv)
 
                     String_View message = {0};
                     params_next(&params, &message);
-
-                    Command_Call command_call = {0};
-                    if (command_call_parse(SV("%"), message, &command_call)) {
-                        Command_Def def = {0};
-                        if (commands_find_def(&commands, command_call.name, &def)) {
-                            irc_privmsg(&irc, channel, def.response);
-                        } else {
-                            log_warning(&log, "Could not find command `"SV_Fmt"`", SV_Arg(command_call.name));
-                        }
-                    }
                 }
             }
 
