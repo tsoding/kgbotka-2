d1353e1ba905961cb81d794e2304eaeda17adbcb
rexim <reximkut@gmail.com>
2021-03-13 14:53:19 +0700

    Split main into distrinct sections

--- src/main.c
+++ src/main.c
@@ -79,35 +79,36 @@ void usage(const char *program, FILE *stream)
 
 int main(int argc, char **argv)
 {
+    // Resource to destroy at the end
     char *secret_conf_content = NULL;
     Irc irc = {0};
 
-    //////////////////////////////
-
     Log log = log_to_handle(stdout);
 
-    const char *const program = shift(&argc, &argv);        // skip program
-
-    if (argc == 0) {
-        usage(program, stderr);
-        log_error(&log, "path to secret.conf file expected");
-        goto error;
-    }
-
-    const char *const secret_conf_path = shift(&argc, &argv);
-
-    secret_conf_content = slurp_file(secret_conf_path);
-    if (secret_conf_content == NULL) {
-        log_error(&log, "Could not read file `%s`: %s",
-                  secret_conf_path, strerror(errno));
-        goto error;
-    }
-
+    // Secret configuration
     String_View nickname = SV_NULL;
     String_View password = SV_NULL;
     String_View channel  = SV_NULL;
 
+    // Parse secret.conf
     {
+        const char *const program = shift(&argc, &argv);        // skip program
+
+        if (argc == 0) {
+            usage(program, stderr);
+            log_error(&log, "path to secret.conf file expected");
+            goto error;
+        }
+
+        const char *const secret_conf_path = shift(&argc, &argv);
+
+        secret_conf_content = slurp_file(secret_conf_path);
+        if (secret_conf_content == NULL) {
+            log_error(&log, "Could not read file `%s`: %s",
+                      secret_conf_path, strerror(errno));
+            goto error;
+        }
+
         String_View content = sv_from_cstr(secret_conf_content);
 
         while (content.count > 0) {
@@ -144,90 +145,96 @@ int main(int argc, char **argv)
         }
     }
 
-    if (!irc_connect(&log, &irc, HOST, PORT)) {
-        goto error;
-    }
-
-    // TODO: no support for Twitch IRC tags
-    irc_pass(&irc, password);
-    irc_nick(&irc, nickname);
-    irc_join(&irc, channel);
-
-    // TODO: autoreconnect
-
-#define BUFFER_DROPS_THRESHOLD 5
-    char buffer[4096];
-    size_t buffer_size = 0;
-
-    int read_size = SSL_read(irc.ssl, buffer + buffer_size, sizeof(buffer) - buffer_size);
-    size_t buffer_drops_count = 0;
-    while (read_size > 0 && buffer_drops_count < BUFFER_DROPS_THRESHOLD) {
-        buffer_size += read_size;
-
-        {
-            String_View buffer_view = {
-                .count = buffer_size,
-                .data = buffer,
-            };
-
-            String_View line = {0};
-            while (sv_try_chop_by_delim(&buffer_view, '\n', &line)) {
-                line = sv_trim(line);
-                log_info(&log, SV_Fmt, SV_Arg(line));
-
-                if (sv_starts_with(line, SV(":"))) {
-                    String_View prefix = sv_chop_by_delim(&line, ' ');
-                    (void) prefix;
-                }
-
-                String_View command = sv_chop_by_delim(&line, ' ');
-
-                String_View params = line;
-
-                if (sv_eq(command, SV("PING"))) {
-                    String_View param = {0};
-                    if (params_next(&params, &param)) {
-                        irc_pong(&irc, param);
-                    } else {
-                        irc_pong(&irc, SV("tmi.twitch.tv"));
-                    }
-                } else if (sv_eq(command, SV("PRIVMSG"))) {
-                    String_View channel = {0};
-                    params_next(&params, &channel);
-
-                    String_View message = {0};
-                    params_next(&params, &message);
-                }
-            }
-
-            if (buffer_view.count == sizeof(buffer)) {
-                // NOTE: if after filling up the buffer completely we still
-                // could not process any valid IRC messages, we assume that server
-                // sent garbage and dropping it.
-                buffer_drops_count += 1;
-                log_warning(&log, "[%zu/%zu] Server sent garbage.",
-                            buffer_drops_count, (size_t) BUFFER_DROPS_THRESHOLD);
-                buffer_size = 0;
-            } else {
-                memmove(buffer, buffer_view.data, buffer_view.count);
-                buffer_size = buffer_view.count;
-                buffer_drops_count = 0;
-            }
+    // Connect to IRC
+    {
+        if (!irc_connect(&log, &irc, HOST, PORT)) {
+            goto error;
         }
 
-        read_size = SSL_read(irc.ssl, buffer + buffer_size, sizeof(buffer) - buffer_size);
+        // TODO: no support for Twitch IRC tags
+        irc_pass(&irc, password);
+        irc_nick(&irc, nickname);
+        irc_join(&irc, channel);
+
+        // TODO: autoreconnect
     }
 
-    if (buffer_drops_count >= BUFFER_DROPS_THRESHOLD) {
-        log_error(&log, "Server sent too much garbage");
-        goto error;
-    }
+    // IRC event loop
+    {
+#define BUFFER_DROPS_THRESHOLD 5
+        char buffer[4096];
+        size_t buffer_size = 0;
 
-    if (read_size <= 0) {
-        char buf[512] = {0};
-        ERR_error_string_n(SSL_get_error(irc.ssl, read_size), buf, sizeof(buf));
-        log_error(&log, "SSL failed with error: %s", buf);
-        goto error;
+        int read_size = SSL_read(irc.ssl, buffer + buffer_size, sizeof(buffer) - buffer_size);
+        size_t buffer_drops_count = 0;
+        while (read_size > 0 && buffer_drops_count < BUFFER_DROPS_THRESHOLD) {
+            buffer_size += read_size;
+
+            {
+                String_View buffer_view = {
+                    .count = buffer_size,
+                    .data = buffer,
+                };
+
+                String_View line = {0};
+                while (sv_try_chop_by_delim(&buffer_view, '\n', &line)) {
+                    line = sv_trim(line);
+                    log_info(&log, SV_Fmt, SV_Arg(line));
+
+                    if (sv_starts_with(line, SV(":"))) {
+                        String_View prefix = sv_chop_by_delim(&line, ' ');
+                        (void) prefix;
+                    }
+
+                    String_View command = sv_chop_by_delim(&line, ' ');
+
+                    String_View params = line;
+
+                    if (sv_eq(command, SV("PING"))) {
+                        String_View param = {0};
+                        if (params_next(&params, &param)) {
+                            irc_pong(&irc, param);
+                        } else {
+                            irc_pong(&irc, SV("tmi.twitch.tv"));
+                        }
+                    } else if (sv_eq(command, SV("PRIVMSG"))) {
+                        String_View channel = {0};
+                        params_next(&params, &channel);
+
+                        String_View message = {0};
+                        params_next(&params, &message);
+                    }
+                }
+
+                if (buffer_view.count == sizeof(buffer)) {
+                    // NOTE: if after filling up the buffer completely we still
+                    // could not process any valid IRC messages, we assume that server
+                    // sent garbage and dropping it.
+                    buffer_drops_count += 1;
+                    log_warning(&log, "[%zu/%zu] Server sent garbage.",
+                                buffer_drops_count, (size_t) BUFFER_DROPS_THRESHOLD);
+                    buffer_size = 0;
+                } else {
+                    memmove(buffer, buffer_view.data, buffer_view.count);
+                    buffer_size = buffer_view.count;
+                    buffer_drops_count = 0;
+                }
+            }
+
+            read_size = SSL_read(irc.ssl, buffer + buffer_size, sizeof(buffer) - buffer_size);
+        }
+
+        if (buffer_drops_count >= BUFFER_DROPS_THRESHOLD) {
+            log_error(&log, "Server sent too much garbage");
+            goto error;
+        }
+
+        if (read_size <= 0) {
+            char buf[512] = {0};
+            ERR_error_string_n(SSL_get_error(irc.ssl, read_size), buf, sizeof(buf));
+            log_error(&log, "SSL failed with error: %s", buf);
+            goto error;
+        }
     }
 
 // ok:
