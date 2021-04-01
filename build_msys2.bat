c0fbb0b2b710003791ca0374901678d26a6d023a
Ciremun <Ciremun@yandex.ru>
2021-04-01 03:38:24 +0300

    remove unused var

--- build_msys2.bat
+++ build_msys2.bat
@@ -4,4 +4,4 @@ set CFLAGS=-Wall -Wextra -std=c11 -pedantic -ggdb
 set SOURCES=src/main.c src/sv.c src/log.c src/irc.c src/cmd.c src/region.c
 set LIBS=-lssl -lcrypto -lcurl -lws2_32
 
-%CC% %CFLAGS% %SOURCES% %INCLUDES% %LIBS% -o kgbotka
+%CC% %CFLAGS% %SOURCES% %LIBS% -o kgbotka
