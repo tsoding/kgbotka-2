37a47af936f3294237ae7cb645489cb5d8938419
rexim <reximkut@gmail.com>
2021-04-15 22:42:31 +0700

    Fix formatting remarks

--- src/discord.c
+++ src/discord.c
@@ -3,18 +3,30 @@
 const char *discord_opcode_as_cstr(Discord_Opcode opcode)
 {
     switch (opcode) {
-    case DISCORD_OPCODE_DISPATCH:      return "DISCORD_OPCODE_DISPATCH";
-    case DISCORD_OPCODE_HEARTBEAT:     return "DISCORD_OPCODE_HEARTBEAT";
-    case DISCORD_OPCODE_IDENTIFY:      return "DISCORD_OPCODE_IDENTIFY";
-    case DISCORD_OPCODE_PRESENCE:      return "DISCORD_OPCODE_PRESENCE";
-    case DISCORD_OPCODE_VOICE:         return "DISCORD_OPCODE_VOICE";
-    case DISCORD_OPCODE_RESUME:        return "DISCORD_OPCODE_RESUME";
-    case DISCORD_OPCODE_RECONNECT:     return "DISCORD_OPCODE_RECONNECT";
-    case DISCORD_OPCODE_REQUEST:       return "DISCORD_OPCODE_REQUEST";
-    case DISCORD_OPCODE_INVALID:       return "DISCORD_OPCODE_INVALID";
-    case DISCORD_OPCODE_HELLO:         return "DISCORD_OPCODE_HELLO";
-    case DISCORD_OPCODE_HEARTBEAT_ACK: return "DISCORD_OPCODE_HEARTBEAT_ACK";
-    default:                           return "(Unknown)";
+    case DISCORD_OPCODE_DISPATCH:
+        return "DISCORD_OPCODE_DISPATCH";
+    case DISCORD_OPCODE_HEARTBEAT:
+        return "DISCORD_OPCODE_HEARTBEAT";
+    case DISCORD_OPCODE_IDENTIFY:
+        return "DISCORD_OPCODE_IDENTIFY";
+    case DISCORD_OPCODE_PRESENCE:
+        return "DISCORD_OPCODE_PRESENCE";
+    case DISCORD_OPCODE_VOICE:
+        return "DISCORD_OPCODE_VOICE";
+    case DISCORD_OPCODE_RESUME:
+        return "DISCORD_OPCODE_RESUME";
+    case DISCORD_OPCODE_RECONNECT:
+        return "DISCORD_OPCODE_RECONNECT";
+    case DISCORD_OPCODE_REQUEST:
+        return "DISCORD_OPCODE_REQUEST";
+    case DISCORD_OPCODE_INVALID:
+        return "DISCORD_OPCODE_INVALID";
+    case DISCORD_OPCODE_HELLO:
+        return "DISCORD_OPCODE_HELLO";
+    case DISCORD_OPCODE_HEARTBEAT_ACK:
+        return "DISCORD_OPCODE_HEARTBEAT_ACK";
+    default:
+        return "(Unknown)";
     }
 }
 
