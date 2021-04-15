ac88174347cb562e3a25c15fb59f370f34219697
rexim <reximkut@gmail.com>
2021-04-15 20:28:01 +0700

    Try to fix MSVC build

--- build_msvc.bat
+++ build_msvc.bat
@@ -5,7 +5,7 @@ if not exist msvc-build mkdir msvc-build
 
 set CC=cl
 set CFLAGS=/FC /nologo /W4
-set SOURCES=src/main.c src/sv.c src/log.c src/irc.c src/cmd.c src/region.c src/http.c src/discord.c src/net.c
+set SOURCES=src/main.c src/sv.c src/log.c src/irc.c src/cmd.c src/region.c src/http.c src/discord.c src/sockets.c
 set INCLUDES=/Ivendor/openssl/include /Ivendor/curl/include
 set LIBS=/DCURL_STATICLIB /link vendor/curl/lib/libcurl_a.lib vendor/openssl/lib/libcrypto.lib vendor/openssl/lib/libssl.lib Ws2_32.lib Wldap32.lib MSVCRT.lib Advapi32.lib Crypt32.lib Normaliz.lib User32.lib
 
