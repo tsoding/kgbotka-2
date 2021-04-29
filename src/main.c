bb0136cc9bff20d2d121304df473d09906ec3ae5
rexim <reximkut@gmail.com>
2021-04-29 23:28:08 +0700

    Try to fix MSVC build

--- src/main.c
+++ src/main.c
@@ -199,7 +199,7 @@ void connect_discord(CURL *curl, Region *memory, Log *log, SSL_CTX *ctx, String_
             goto error;
         }
 
-        log_info(log, "HEARTBEAT INTERVAL: %"PRIu64, payload.d.hello.heartbeat_interval);
+        log_info(log, "HEARTBEAT INTERVAL: %" PRIu64, payload.d.hello.heartbeat_interval);
     }
 
     // Sending Identify
